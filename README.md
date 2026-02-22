6af57493eab7b9801e9e2ccc5a8f0ab6b4d093c9
yassinech26 <cherifyassinee@example.com>
2026-02-22 16:41:12 +0100

    ajout spring projet

--- README.md
+++
@@ -1,2 +0,0 @@
-# SwapTN
-PROJET FEDERE
---
+++ src/test/java/com/CherifYedesBenhamedHemdene/SwapTN/SwapTnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CherifYedesBenhamedHemdene.SwapTN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapTnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
